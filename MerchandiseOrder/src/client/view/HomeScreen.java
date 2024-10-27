5010e62a205b708bc242e24ebb4285a0bdaa24a3
Ngô Trung Tuyên <trungtuyen2003@vnpt.vn>
2024-10-27 22:24:21 +0700

    update fix 27/10/2024

--- MerchandiseOrder/src/client/view/HomeScreen.java
+++ MerchandiseOrder/src/client/view/HomeScreen.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class HomeScreen extends JPanel {
 
     public HomeScreen() {
-        JLabel lobbyLabel = new JLabel("Welcome to the Client.Controller.Client Lobby");
+        JLabel lobbyLabel = new JLabel("Welcome to the Client.controller.Client Lobby");
         this.add(lobbyLabel);
     }
 }
